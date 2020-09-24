67d01a7f710dcef8077bccbfe10ce4261c824ecc
Thomson <thomson.sun@lifebyte.io>
2020-09-24 16:23:31 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import i18n from "./i18n/index.js";
+import { Select, Option } from "element-ui";
+
+Vue.config.productionTip = false;
+Vue.use(Select).use(Option);
+
+new Vue({
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+}).$mount("#app");
