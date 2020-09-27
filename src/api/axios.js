95fb4db6527e6167e4e4bd91eeaea6df880e326d
thomson00 <thomson.sun@lifebyte.io>
2020-09-27 18:57:42 +0800

    feat(internal): 优化

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,13 +3,14 @@ import qs from 'qs';
 import store from '../store/index.js';
 import ACCEPT from '../utils/accept.js';
 axios.defaults.baseURL = process.env.VUE_APP_API;
+const mockUrl = 'https://dev-mock.tm-nonprod.com/mock/5f5a09c5d39fdf2dd922d150';
 
 /**
  * @param {*} url 请求url
  * @param {*} [params={}] 请求参数
  * @param {string} [method='post'] 请求方法
  */
-export const reqJsonData = async ({ url, params = {}, method = 'post', isformData = false, accept = 'prs-v2' }) => {
+export const reqJsonData = async ({ url, params = {}, method = 'post', mock = false, isformData = false, accept = 'prs-v2' }) => {
     const config = {
         url,
         params,
@@ -23,6 +24,9 @@ export const reqJsonData = async ({ url, params = {}, method = 'post', isformDat
         config.params = params;
     } else {
         config.data = qs.stringify(params);
+    }
+    if (mock) {
+        config.baseURL = mockUrl;
     }
     return new Promise(resolve => {
         try {
